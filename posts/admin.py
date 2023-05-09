98a0a43b14b4464c8a06b2cc90cc85200c84190c
minjeong <ehdro418@naver.com>
2023-05-09 13:35:48 +0900

    카테고리 모델 추가

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import Category, Post, Comment
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
--- posts/models.py
+++ posts/models.py
@@ -8,14 +8,14 @@ class Category(models.Model):
     def __str__(self):
         return str(self.name)
 
-class Post(models.Model):    
+class Post(models.Model):  
+    category = models.ForeignKey(Category, verbose_name="카테고리", on_delete=models.CASCADE)  
     title = models.CharField("제목", max_length=100)
     content = models.TextField("내용")
     image = models.ImageField("사진", blank=True, null=True, upload_to='%Y/%m/%d')
     star = models.IntegerField("별점", validators=[MinValueValidator(1), MaxValueValidator(5)])
     created_at = models.DateField("생성일", auto_now_add=True)
-    updated_at = models.DateField("마지막 수정일", auto_now=True)
-    category = models.ForeignKey(Category, verbose_name="카테고리", on_delete=models.CASCADE)
+    updated_at = models.DateField("마지막 수정일", auto_now=True)    
     like = models.ManyToManyField(User, verbose_name="좋아요", related_name='like_posts')
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts")
