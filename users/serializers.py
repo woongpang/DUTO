3c24ccdacef40dba42d784c9c1b00857d6e1d3e3
hanilcome <hanilcome@naver.com>
2023-05-09 19:27:30 +0900

    user login & user follow

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,21 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.models import User
+from django.contrib.auth.hashers import make_password
+
+class UserSerializer(serializers.ModelSerializer):
+    followings = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = User
+        fields = "__all__"
+    
+    # 회원가입시 비밀번호를 해싱(암호화) 하는 함수
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
 class LoginViewSerializer(TokenObtainPairSerializer):
+    
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
